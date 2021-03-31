fe3048fce57d011936444c650fabafc3c0a84f6a
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-03-31 16:46:55 +0530

    Add project files.

---
+++ CFSP/Models/CFSPContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CFSP.Models
+{
+    public class CFSPContext:DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                        .MapToStoredProcedures();
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CFSP/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CFSP.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DoB { get; set; }
+    }
+}
